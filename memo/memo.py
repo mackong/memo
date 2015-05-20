5afed12a931a093ca680360826c4b0ae01a86b0d
mackong <mackonghp@gmail.com>
2015-05-20 09:31:15 +0800

    Add delete note. Add organize note. Fix some pylint issues

--- memo/memo.py
+++ memo/memo.py
@@ -28,7 +28,7 @@ class NoteStatus:
     DONE = 'D'
     UNDONE = 'U'
     POSTPONED = 'P'
-    
+
 
 class Note:
     """This representaion a note taken.
@@ -47,25 +47,25 @@ class Note:
 
     def __str__(self):
         return '%d\t%s\t%s\t%s' % (self.id,
-                                      self.status,
-                                      self.date,
-                                      self.content.rstrip('\n'))
+                                   self.status,
+                                   self.date,
+                                   self.content.rstrip('\n'))
 
     def trim_date(self):
         """Get the note string without date attribute."""
         return '%d\t%s\t%s' % (self.id,
-                                 self.status,
-                                 self.content.rstrip('\n'))
+                               self.status,
+                               self.content.rstrip('\n'))
 
-    
+
 def is_odd(x):
     """Return True if x is odd, False if x is even."""
     return x % 2
 
 
 def get_current_date():
-    """Get current date in format yyyy-MM-dd"""    
-    return time.strftime('%Y-%m-%d')    
+    """Get current date in format yyyy-MM-dd"""
+    return time.strftime('%Y-%m-%d')
 
 
 def get_memorc_path():
@@ -78,11 +78,14 @@ def get_memo_conf_value(prop):
 
     The .memorc file format is like `prop=value`.
     """
-    with open(get_memorc_path(), 'r') as f:
-        for line in f.read().splitlines():
-            tokens = [x.strip() for x in line.split('=')]
-            if tokens[0] == prop:
-                return tokens[1]
+    try:
+        with open(get_memorc_path(), 'r') as f:
+            for line in f.read().splitlines():
+                tokens = [x.strip() for x in line.split('=')]
+                if tokens[0] == prop:
+                    return tokens[1]
+    except IOError:
+        return None
 
     return None
 
@@ -96,7 +99,7 @@ def get_memo_path():
 
 
 def get_next_memo_id():
-    """Get the next new note id."""    
+    """Get the next new note id."""
     notes = get_all_notes()
     if not len(notes):
         return 1
@@ -118,35 +121,48 @@ def get_all_notes():
             notes.append(note)
 
         return notes
-    
+
 
 def print_version(ctx, param, value):
+    """Print memo version."""    
     if not value or ctx.resilient_parsing:
         return
-    
+
     print('Memo version: ' + colored(__version__, 'red'))
     ctx.exit()
 
 
 def print_memo_path(ctx, param, value):
+    """Print memo path."""
     if not value or ctx.resilient_parsing:
         return
 
     print('Memo path: ' + colored(get_memo_path(), 'red'))
-    ctx.exit()    
+    ctx.exit()
 
 
 def organize_notes(ctx, param, value):
+    """Reorder and organize the note id.
+
+    If we already have note 1, 2, 3 and delete note 2, then we have note 1, 3.
+    Now if we organize them, we'll get note 1, 2.
+    """
     if not value or ctx.resilient_parsing:
         return
 
+    notes = get_all_notes()
+    with open(get_memo_path(), 'w') as f:
+        for i, note in enumerate(notes):
+            note.id = i + 1  # note id start by 1.
+            f.write(str(note) + '\n')
+    
     ctx.exit()
 
 
 def add_note(content, date):
     with open(get_memo_path(), 'a') as f:
         note = Note(get_next_memo_id(), NoteStatus.UNDONE, date, content)
-        f.write(note)
+        f.write(str(note) + '\n')
 
 
 def add_note_from_input(input):
@@ -164,7 +180,7 @@ def validate_date(ctx, param, value):
     the date format must be in format yyyy-MM-dd, ex. 2014-12-01.
     """
     error_msg = 'date must be in format yyyy-MM-dd'
-    
+
     try:
         tokens = [int(x) for x in value.split('-')]
         if len(tokens) != 3:
@@ -174,7 +190,7 @@ def validate_date(ctx, param, value):
         day_count = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
         if year % 400 == 0 or (year % 100 != 0 and year % 4 == 0):
-            day_count[1] = 29;
+            day_count[1] = 29
 
         if month < 13 and month > 0:
             if day < day_count[month - 1]:
@@ -185,7 +201,7 @@ def validate_date(ctx, param, value):
             raise click.BadParameter(error_msg)
     except ValueError:
         raise click.BadParameter(error_msg)
-            
+
 
 @click.command()
 @click.option('--content', '-c', metavar='content',
@@ -208,7 +224,7 @@ def get_line_color(is_odd_line):
     Return None if USE_COLORS is set to false.
     """
     use_colors = get_memo_conf_value('USE_COLORS')
-    
+
     if not use_colors or use_colors == 'no':
         return None
 
@@ -220,17 +236,20 @@ def get_line_color(is_odd_line):
 
 
 def dump_note(note, is_odd_line):
-    cprint(note, get_line_color(is_odd_line))
+    """Dump the note with line color."""
+    cprint(str(note), get_line_color(is_odd_line))
 
 
 def dump_organized(date, notes, is_odd_line):
+    """Dump the notes by date organized with line color."""
     color = get_line_color(is_odd_line)
     cprint(date, color)
     for note in notes:
         cprint('\t%s' % note.trim_date(), color)
-    
+
 
 def show_notes():
+    """Show all notes."""
     notes = get_all_notes()
     if len(notes):
         for i, note in enumerate(notes):
@@ -238,15 +257,19 @@ def show_notes():
     else:
         cprint('You don\'t have any notes currently.', 'red')
 
+
 def show_latest(n):
+    """Show latest n notes."""
     notes = get_all_notes()
     if len(notes):
         for i, note in enumerate(notes[n + 1:]):
             dump_note(note, is_odd(i))
     else:
         cprint('You don\'t have any notes currently.', 'red')
-            
+
+
 def show_organized():
+    """Show organized notes by date."""
     notes = get_all_notes()
     if len(notes):
         organized = {}
@@ -261,7 +284,9 @@ def show_organized():
     else:
         cprint('You don\'t have any notes currently.', 'red')
 
+
 def show_unpostponed():
+    """Show all notes except POSTPONED."""
     notes = get_all_notes()
     if len(notes):
         for i, note in enumerate(notes):
@@ -270,7 +295,9 @@ def show_unpostponed():
     else:
         cprint('You don\'t have any notes currently.', 'red')
 
+
 def show_undone():
+    """Show all notes except UNDONE."""
     notes = get_all_notes()
     if len(notes):
         for i, note in enumerate(notes):
@@ -278,8 +305,8 @@ def show_undone():
                 dump_note(note, is_odd(i))
     else:
         cprint('You don\'t have any notes currently.', 'red')
-        
-            
+
+
 @click.command()
 @click.option('--latest', '-l', default=0, metavar='n',
               help='Show latest n notes.')
@@ -302,13 +329,35 @@ def show(latest, organized, unpostponed, undone):
     else:
         show_notes()
 
+
+def delete_all_notes():
+    """Delete all notes.
+
+    Because memo will create the .memo file when start,
+    So we can simply delete the .memo file to delete all notes.
+    """
+    os.remove(get_memo_path())
+
+
+def delete_note(id):
+    """Delete note by given id."""
+    notes = get_all_notes()
+    with open(get_memo_path(), 'w') as f:
+        for note in notes:
+            if note.id != id:
+                f.write(str(note) + '\n')
+
+
 @click.command()
 @click.option('--all', '-a', is_flag=True,
               help='Delete all notes.')
-@click.argument('id', required=False)
+@click.argument('id', required=False, type=int)
 def delete(all, id):
     """Delete note by id or delete all notes."""
-    print(all, id)
+    if all:
+        delete_all_notes()
+    else:
+        delete_note(id)
 
 # Context setting for Click command.
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
